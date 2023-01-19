d1bb450471132fda8c654097473380e61990aa3a
Kanahiro <mediterranean1769@gmail.com>
2023-01-19 14:59:21 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), vue()]
+    integrations: [react(), svelte(), vue()],
+    base: './astro-maplibraries',
 });
